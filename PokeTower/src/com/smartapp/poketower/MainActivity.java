c7a4017af2c2f9d1271500014d40b3ccf0bf4029
Clement DREYFUSS <clement.dreyfuss@epitech.eu>
2012-11-18 14:02:02 +0900

    Add of the maps and display

--- PokeTower/src/com/smartapp/poketower/MainActivity.java
+++ PokeTower/src/com/smartapp/poketower/MainActivity.java
@@ -1,20 +1,73 @@
 package com.smartapp.poketower;
 
+import java.io.IOException;
+
+import android.content.res.Resources.NotFoundException;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import map.Map;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+import display.TowerDisplay;
+
+import spawner.Spawner;
+
+public class MainActivity extends SimpleBaseGameActivity {
+
+	private Map _map = null;
+	private TowerDisplay _towerDisplay = null;
+	private Spawner _spawner = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		_towerDisplay = new TowerDisplay();
+		_spawner = new Spawner();
+		_spawner.loadSpawner(this.getResources().openRawResource(R.raw.spawn1));
+		try {
+			this._map = new Map();
+			this._map.loadMap(this.getResources().openRawResource(R.raw.map1));
+		} catch (NotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_main);
+	}
+
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		getMenuInflater().inflate(R.menu.activity_main, menu);
+//		return true;
+//	}
+
+	public EngineOptions onCreateEngineOptions() {
+		final Camera camera = new Camera(0, 0, this._map.getWidth(), this._map.getHeight());
+		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, 
+				new RatioResolutionPolicy(this._map.getWidth(), this._map.getHeight()), camera);
+	}
+
+	@Override
+	protected void onCreateResources() {
+		this._map.loadRessources(this);
+		this._towerDisplay.loadRessources(this);
+	}
+
+	@Override
+	protected Scene onCreateScene() {
+		final Scene scene = new Scene();
+
+		this._map.displayMap(this, scene);
+		this._towerDisplay.addTower(this, scene, "Bulbasaur", 0, 0);
+		this._towerDisplay.addTower(this, scene, "Ivysaur", 0, 1);
+		this._towerDisplay.addTower(this, scene, "Venusaur", 0, 2);
+		this._towerDisplay.addTower(this, scene, "Oddish", 0, 4);
+		return scene;
+	}
 }
--- PokeTower/src/map/Map.java
+++ PokeTower/src/map/Map.java
@@ -1,26 +1,55 @@
 package map;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class Map {
-	
-	private List<String> _map;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.scene.Scene;
+import org.andengine.opengl.texture.ITexture;
+import org.andengine.opengl.texture.bitmap.BitmapTexture;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.texture.region.TextureRegionFactory;
+import org.andengine.util.adt.io.in.IInputStreamOpener;
+import org.andengine.util.debug.Debug;
 
-	public void loadMap(String path)
+import com.smartapp.poketower.MainActivity;
+
+import display.TowerDisplay;
+
+public class Map {
+
+	//Lists
+	private List<String> _map;
+	private List<Sprite> _sprites;
+
+	//Textures
+	private ITextureRegion _TForbidden;
+	private ITextureRegion _TBorder;
+	private ITextureRegion _TPrincess;
+	private ITextureRegion _TSpawner;
+	private ITextureRegion _TPath;
+	private ITextureRegion _TTowerPlace;
+	private final int SQUARE_SIZE = 16;
+
+	public Map()
+	{
+		_map = new ArrayList<String>();
+		_sprites = new ArrayList<Sprite>();
+	}
+
+	public void loadMap(InputStream inputStream)
 	{
 		BufferedReader buff = null;
+		InputStreamReader ipsr = null;
 		String str = null;
 
-		try {
-			buff = new BufferedReader(new FileReader("path_to_file"));
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+		ipsr = new InputStreamReader(inputStream);
+		buff = new BufferedReader(ipsr);
 		try {
 			while((str = buff.readLine()) != null){
 				_map.add(str);
@@ -51,12 +80,88 @@ public class Map {
 		x = -1;
 		y = -1;
 	}
-		
+
 	public boolean canPutTowerAt(int x, int y)
 	{
 		if (_map.get(y).charAt(x) == ' ')
 			return true;
 		return false;		
+	}
+
+	public int getHeight()
+	{
+		return (_map.size() * SQUARE_SIZE);
+	}
+
+	public int getWidth()
+	{
+		if (_map.size() == 0)
+			return 0;
+		return (_map.get(0).length() * SQUARE_SIZE);
+	}
+
+	public void loadRessources(final MainActivity main)
+	{		
+		ITexture TForbidden = TowerDisplay.loadTexture(main, "gfx/Forbidden.png");
+		this._TForbidden = TextureRegionFactory.extractFromTexture(TForbidden);
+
+		ITexture TPrincess = TowerDisplay.loadTexture(main, "gfx/Princess.png");
+		this._TPrincess = TextureRegionFactory.extractFromTexture(TPrincess);
+
+		ITexture TSpawner = TowerDisplay.loadTexture(main, "gfx/Spawner.png");
+		this._TSpawner = TextureRegionFactory.extractFromTexture(TSpawner);
+
+		ITexture TBorder = TowerDisplay.loadTexture(main, "gfx/Border.png");
+		this._TBorder = TextureRegionFactory.extractFromTexture(TBorder);
+
+		ITexture TPath = TowerDisplay.loadTexture(main, "gfx/Path.png");
+		this._TPath = TextureRegionFactory.extractFromTexture(TPath);
+
+		ITexture TTowerPlace = TowerDisplay.loadTexture(main, "gfx/TowerPlace.png");
+		this._TTowerPlace = TextureRegionFactory.extractFromTexture(TTowerPlace);
+	}
+
+	public void displayMap(MainActivity main, Scene scene)
+	{
+		Sprite sprite;
+		int x, y;
+
+
+		for (y = 0; y < _map.size(); y++)
+			for (x = 0; x < _map.get(y).length(); x++)
+			{
+				sprite = null;
+				if (_map.get(y).charAt(x) == 'X')
+					sprite = new Sprite(x * SQUARE_SIZE, y * SQUARE_SIZE, this._TForbidden, main.getVertexBufferObjectManager());
+				else if (_map.get(y).charAt(x) == 'P')
+					sprite = new Sprite(x * SQUARE_SIZE, y * SQUARE_SIZE, this._TPrincess, main.getVertexBufferObjectManager());
+				else if (_map.get(y).charAt(x) == 'S')
+					sprite = new Sprite(x * SQUARE_SIZE, y * SQUARE_SIZE, this._TSpawner, main.getVertexBufferObjectManager());
+				else if (_map.get(y).charAt(x) == 'B')
+					sprite = new Sprite(x * SQUARE_SIZE, y * SQUARE_SIZE, this._TBorder, main.getVertexBufferObjectManager());
+				else if (_map.get(y).charAt(x) == '.')
+					sprite = new Sprite(x * SQUARE_SIZE, y * SQUARE_SIZE, this._TPath, main.getVertexBufferObjectManager());
+				else if (_map.get(y).charAt(x) == ' ')
+					sprite = new Sprite(x * SQUARE_SIZE, y * SQUARE_SIZE, this._TTowerPlace, main.getVertexBufferObjectManager());
+				if (sprite != null)
+				{
+					scene.attachChild(sprite);
+					_sprites.add(sprite);
+				}
+			}
+	}
+	
+	public void clearMap(Scene scene)
+	{
+		Sprite sprite;
+		Iterator<Sprite> it = _sprites.iterator();
+		while (it.hasNext())
+		{
+			sprite = it.next();
+			scene.detachChild(sprite);
+		}
+		_sprites.clear();
+		_map.clear();
 	}
 
 }
